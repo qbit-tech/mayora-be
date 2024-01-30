86d6742824c2f2998f6e4062b4733ef1e0ac1941
Kitsuzune <teguhwmn189@gmail.com>
2024-01-30 17:54:24 +0700

    Change Release To Auto Incerement

--- apps/mayora/src/app.module.ts
+++ apps/mayora/src/app.module.ts
@@ -106,7 +106,7 @@ export const rootImportedModules = [
   RedisModule.forRoot(redisOption),
   SequelizeModule.forRoot({
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || '12345',
+    password: process.env.DB_PASS || '123456',
     database: process.env.DB_NAME || 'mayora',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
